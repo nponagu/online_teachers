f5da5a92023c0220a91257f4a0d74dc3fc2194c5
Alexander Tikhonov <nponagu@ya.ru>
2020-06-10 14:55:45 +0300

    add rendering templates

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/goals/<goal>/')
 def render_goal(goal):
-    return 'Hello Moon!'
+    return render_template('goal.html')
 
 
 @app.route('/profiles/<int:teacher_id>/')
 def render_profile(teacher_id):
-    return 'Hello World!'
+    return render_template('profile.html')
 
 
 @app.route('/request/')
 def render_request():
-    return 'Hello World!'
+    return render_template('request.html')
 
 
 @app.route('/request_done/')
 def render_request_done():
-    return 'Hello World!'
+    return render_template('request_done.html')
 
 
 @app.route('/booking/<int:teacher_id>/<int:day>/<time>/')
 def render_booking(teacher_id, day, time):
-    return 'Hello World!'
+    return render_template('booking.html')
 
 
 @app.route('/booking_done/')
 def render_booking_done():
-    return 'Hello World!'
+    return render_template('booking_done.html')
 
 
 if __name__ == '__main__':
