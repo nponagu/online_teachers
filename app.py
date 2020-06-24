b928fc18c705f448a59e5d265c343b79e8b0f234
Alexander Tikhonov <nponagu@ya.ru>
2020-06-24 13:52:41 +0300

    a bad structure but everything works

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import Teacher
 
 app = Flask(__name__)
 
@@ -18,6 +17,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_connection
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+class Teacher(db.Model):
+    __tablename__ = 'teachers'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(50), nullable=False)
+    about = db.Column(db.Text)
+    rating = db.Column(db.Float)
+    picture = db.Column(db.Text)
+    price = db.Column(db.Integer, nullable=False)
+    goals = db.Column(db.Text)
+    free = db.Column(db.JSON)
+    bookings = db.relationship("Booking")
+
+
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    clientName = db.Column(db.String(50), nullable=False)
+    clientPhone = db.Column(db.String(15), nullable=False)
+    clientTime = db.Column(db.String(5), nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey("teachers.id"))
+    teacher = db.relationship("Teacher")
+
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    clientName = db.Column(db.String(50), nullable=False)
+    clientPhone = db.Column(db.String(15), nullable=False)
+    goal = db.Column(db.Text)
+    time = db.Column(db.String(5))
+
+db.create_all()
 
 week_days = {
     "mon": "Monday",
@@ -58,12 +89,9 @@ def render_goal(goal):
 @app.route('/profiles/<int:teacher_id>/')
 def render_profile(teacher_id):
 
-    teacher = db.session.query(Teacher).get_or_404(id)
-    # for item in teachers:
-    #     if item["id"] == teacher_id:
-    #         teacher = item
+    teacher = db.session.query(Teacher).get_or_404(teacher_id)
 
-    schedule = teacher["free"]
+    schedule = teacher.free
     return render_template('profile.html',
                            teacher=teacher,
                            schedule=schedule)
